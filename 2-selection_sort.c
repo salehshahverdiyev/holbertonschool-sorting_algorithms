5c58d21ec69d15cba09789bce2ebcbfb702a2f46
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-01-18 07:12:34 -0800

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+void
+swap(int *array, size_t first, size_t second)
+{
+	int tmp = array[second];
+
+	array[second] = array[first];
+	array[first] = tmp;
+}
+
+
+void
+selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+
+	for (i = 0; i < size - 1; ++i)
+	{
+		min = i;
+		for (j = i; j < size - 1; ++j)
+			if (array[min] > array[j])
+				min = j;
+
+		if (min == i)
+			continue;
+
+		swap(array, i , min);
+		print_array(array, size);
+	}
+}
